1004b938421b267c1eb6dbbb8fa64bd46041bd0c
Wahid-Rayas <59963576+Wahid-Rayas@users.noreply.github.com>
2022-03-12 00:22:57 +0100

    Add files via upload

---
+++ TP_01/Personne.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_01
+{
+    internal class Personne
+    {
+        private string _prenom;
+        private string _nom;
+        private DateTime _date;
+        
+        public string Prenom
+        {
+            get { return _prenom; }
+            set
+            {
+                if (value != _prenom)
+                {
+                    _prenom = value;  
+                }
+            }
+        }
+
+        public string Nom
+        {
+            get { return _nom; }
+            set
+            {
+                if (value != _nom)
+                {
+                    _nom = value;
+                }
+            }
+        }
+
+        
+
+    }
+}
