b2fed4f9473c229a0697200df6d913a2717078e5
Wahid-Rayas <59963576+Wahid-Rayas@users.noreply.github.com>
2022-03-12 00:20:30 +0100

    Add files via upload

---
+++ TP_01/~AutoRecover.Personne.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum _genre 
+{ 
+    Homme,
+    Femme
+}
+
+namespace TP_01
+{
+    internal class Personne
+    {
+        private string _prenom;
+        private string _nom;
+        private 
+        private DateTime _dateNaissance;
+        
+        public string Prenom
+        {
+            get { return _prenom; }
+            set
+            {
+                if (value != _prenom)
+                {
+                    _prenom = value;  
+                }
+            }
+        }
+
+        public string Nom
+        {
+            get { return _nom; }
+            set
+            {
+                if (value != _nom)
+                {
+                    _nom = value;
+                }
+            }
+        }
+
+        public dateTime 
+
+        
+
+    }
+}
